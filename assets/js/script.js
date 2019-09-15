7aac6304970dee39583540407e1f1d6f09a0ff27
Cohen <dcoco91@gmail.com>
2019-09-15 11:13:59 -0400

    changed some of the transition values to make the transistions smoother

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,6 @@
 let port = $(".port-el");
+let notRun = true;
+
 
 $(document).ready(function() {
 
@@ -14,9 +16,12 @@ $(document).ready(function() {
     // call remove, then based on ID active is removed and correct area is made active
     links.on("click", function() {
 
-
+        // id's of clicked elements
         let area = $(this).attr("id");
-        remove();
+
+        // checking to see if area is a truthy value, because the li with my name returns undefined
+        if (area) { remove(); }
+
         switch (area) {
             case "homepage":
                 $(".about").removeClass("not-active").addClass("active slide-in-left");
@@ -38,19 +43,33 @@ $(document).ready(function() {
 
 
     function navFix() {
-
-        if (window.scrollY >= top) {
-            $("body").css({ "padding-top": top });
-            nav.addClass("fixed");
-            head.css({ "display": "none" });
-            $("body").addClass("fixed-nav");
+        // if it has not been run yet
+        if (notRun) {
+            // if the scroll is past the nav
+            if (window.scrollY >= top) {
+                $("body").css({ "padding-top": top });
+                nav.addClass("fixed");
+                head.css({ "display": "none" });
+                $("body").addClass("fixed-nav");
+                window.scroll({
+                    top: -100,
+                    left: 0,
+                    behavior: 'smooth'
+                });
+                notRun = false;
+            } else {
+                $("body").css({ "padding-top": 0 });
+                nav.removeClass("fixed");
+                $("body").removeClass("fixed-nav");
+                head.css({ "display": "" });
+            }
 
         } else {
             $("body").css({ "padding-top": 0 });
-            nav.removeClass("fixed");
-            $("body").removeClass("fixed-nav");
-            head.css({ "display": "" });
         }
+
+
+
     }
     // port.on("click", function() {
     //     $().slideToggle("slow");
